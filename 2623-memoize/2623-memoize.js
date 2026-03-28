0cf3b998e180325e9a330dc56a1e69cb692ea2c4
Mekdes Merga <mekdesmerga19@gmail.com>
2026-03-28 09:28:29 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,24 @@
+function memoize(fn) {
+    const cache = new Map();
+    let callCount = 0;
+
+    const memoized = function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        callCount++;
+        const result = fn(...args);
+        cache.set(key, result);
+
+        return result;
+    };
+
+    memoized.getCallCount = function () {
+        return callCount;
+    };
+
+    return memoized;
+}
