46cf941e485f4703f35444b5772fae5629e8baa8
Mekdes Merga <mekdesmerga19@gmail.com>
2026-09-11 23:04:50 +0300

    Time: 0 ms (100%), Space: 53.5 MB (82.81%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+function longestCommonPrefix(strs) {
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+
+            if (prefix === "") {
+                return "";
+            }
+        }
+    }
+
+    return prefix;
+}
